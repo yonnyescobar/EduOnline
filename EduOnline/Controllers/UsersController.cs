5ff546095ef442f10d5d4374e22f6cd68d91e9d7
yonnyescobar@gmail.com <yonnyescobar@gmail.com>
2023-06-06 10:17:53 -0500

    Created the index of users management to administrator role.

---
+++ EduOnline/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using EduOnline.DAL;
+using EduOnline.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace EduOnline.Controllers
+{
+    [Authorize(Roles = "Administrador")]
+    public class UsersController : Controller
+    {
+        #region Dependencies & Properties
+        private readonly IUserHelper _userHelper;
+        private readonly DatabaseContext _context;
+        private readonly IDropDownListsHelper _ddlHelper;
+        private readonly IAzureBlobHelper _azureBlobHelper;
+
+        public UsersController(IUserHelper userHelper, DatabaseContext context, IDropDownListsHelper dropDownListsHelper, IAzureBlobHelper azureBlobHelper)
+        {
+            _userHelper = userHelper;
+            _context = context;
+            _ddlHelper = dropDownListsHelper;
+            _azureBlobHelper = azureBlobHelper;
+        }
+
+        #endregion
+
+        #region Methods & Actions
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Users
+                .Include(u => u.City)
+                .ThenInclude(c => c.State)
+                .ThenInclude(s => s.Country)
+                .ToListAsync());
+        }
+
+        #endregion
+
+    }
+}
