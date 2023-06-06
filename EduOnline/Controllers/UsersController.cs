b009158f42d3679c612f83917117ec70127ccdf6
yonnyescobar@gmail.com <yonnyescobar@gmail.com>
2023-06-06 11:00:37 -0500

    Implemented the action to register to user administrator.

--- EduOnline/Controllers/UsersController.cs
+++ EduOnline/Controllers/UsersController.cs
@@ -1,5 +1,8 @@
 ﻿using EduOnline.DAL;
+using EduOnline.DAL.Entities;
+using EduOnline.Enums;
 using EduOnline.Helpers;
+using EduOnline.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -35,6 +38,82 @@ namespace EduOnline.Controllers
                 .ThenInclude(c => c.State)
                 .ThenInclude(s => s.Country)
                 .ToListAsync());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateAdmin()
+        {
+            Guid emptyGuid = new Guid();
+
+            AddUserViewModel addUserViewModel = new()
+            {
+                Id = Guid.Empty,
+                Countries = await _ddlHelper.GetDDLCountriesAsync(),
+                States = await _ddlHelper.GetDDLStatesAsync(emptyGuid),
+                Cities = await _ddlHelper.GetDDLCitiesAsync(emptyGuid),
+                UserType = UserType.Administrador,
+            };
+
+            return View(addUserViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateAdmin(AddUserViewModel addUserViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid imageId = Guid.Empty;
+
+                if (addUserViewModel.ImageFile != null)
+                    imageId = await _azureBlobHelper.UploadAzureBlobAsync(addUserViewModel.ImageFile, "users");
+
+                addUserViewModel.PhotoId = imageId;
+                addUserViewModel.CreatedDate = DateTime.Now;
+
+                User user = await _userHelper.AddUserAsync(addUserViewModel);
+                if (user == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Este correo ya está siendo usado.");
+                    await FillDropDownListLocation(addUserViewModel);
+                    return View(addUserViewModel);
+                }
+
+                return RedirectToAction("Index", "Users");
+            }
+
+            await FillDropDownListLocation(addUserViewModel);
+            return View(addUserViewModel);
+        }
+
+        private async Task FillDropDownListLocation(AddUserViewModel addUserViewModel)
+        {
+            addUserViewModel.Countries = await _ddlHelper.GetDDLCountriesAsync();
+            addUserViewModel.States = await _ddlHelper.GetDDLStatesAsync(addUserViewModel.CountryId);
+            addUserViewModel.Cities = await _ddlHelper.GetDDLCitiesAsync(addUserViewModel.StateId);
+        }
+
+        [HttpGet]
+        public JsonResult GetStates(Guid countryId)
+        {
+            Country country = _context.Countries
+                .Include(c => c.States)
+                .FirstOrDefault(c => c.Id == countryId);
+
+            if (country == null) return null;
+
+            return Json(country.States.OrderBy(d => d.Name));
+        }
+
+        [HttpGet]
+        public JsonResult GetCities(Guid stateId)
+        {
+            State state = _context.States
+                .Include(s => s.Cities)
+                .FirstOrDefault(s => s.Id == stateId);
+            if (state == null) return null;
+
+            return Json(state.Cities.OrderBy(c => c.Name));
         }
 
         #endregion
